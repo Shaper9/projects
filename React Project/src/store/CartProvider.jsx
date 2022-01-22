3371f7379be1951236c4acf55f6d497cd495a216
Shaper9 <marko.krstin9@gmail.com>
2022-01-22 14:01:30 +0100

    demo finished

--- React Project/src/store/CartProvider.jsx
+++ React Project/src/store/CartProvider.jsx
@@ -9,12 +9,48 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     switch (action.type) {
+
         case "ADD":
-            const updatedItems = state.items.concat(action.payload)
-            const updatedTotalAmount = state.totalAmount + action.payload.price * action.payload.amount;
+            const updatedTotalAmount =
+                state.totalAmount + action.item.price * action.item.amount;
+
+            const existingCartItemIndex = state.items.findIndex(
+                (item) => item.id === action.item.id
+            );
+            const existingCartItem = state.items[existingCartItemIndex];
+            let updatedItems;
+
+            if (existingCartItem) { /*  ovaj if ako item vec postoji */
+                const updatedItem = {
+                    ...existingCartItem,
+                    amount: existingCartItem.amount + action.item.amount,
+                };
+                updatedItems = [...state.items];
+                updatedItems[existingCartItemIndex] = updatedItem;
+            } else {  /*  ovaj selse je ako item ne postoji */
+                updatedItems = state.items.concat(action.item);
+            }
+
             return {
                 items: updatedItems,
-                totalAmount: updatedTotalAmount
+                totalAmount: updatedTotalAmount,
+            };
+
+        case "REMOVE":
+            const existingCartItemIndex2 = state.items.findIndex((item) => item.id === action.id);
+            const existingItem = state.items[existingCartItemIndex2]
+            const updatedTotalAmount2 = state.totalAmount - existingItem.price
+            let updatedItems2;
+            if (existingItem.amount === 1) {  /* ako imamo samo jedan od ovog itema u cartu , izbrisati ga */
+                updatedItems2 = state.items.filter(item => item.id !== action.id)
+            } else { /*  ako imamo vise od 1 itema u cartu te vrste */
+                const updatedItem = { ...existingItem, amount: existingItem.amount - 1 }
+                updatedItems2 = [...state.items];
+                updatedItems2[existingCartItemIndex2] = updatedItem;
+            }
+            return {
+                items: updatedItems2,
+                totalAmount: updatedTotalAmount2
             }
     }
     return defaultCartState
@@ -25,11 +61,11 @@ const CartProvider = (props) => {
     const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
     const addItemToCartHandler = item => {
-        dispatchCartAction({ type: 'ADD', payload: item })
+        dispatchCartAction({ type: 'ADD', item: item })
     }
 
     const removeItemFromCartHandler = id => {
-        dispatchCartAction({ type: 'REMOVE', payload: id })
+        dispatchCartAction({ type: 'REMOVE', id: id })
     }
 
     const cartContext = {
