8195fd236215bea9dbe3ce69ca275e043b236f88
markokrstin <marko.krstin9@gmail.com>
2022-03-09 18:50:45 +0100

    deleted files that have no use

--- ToDo React app/src/index.js
+++ ToDo React app/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthContextProvider } from './components/context/AuthContext';
+import { AuthContextProvider } from './components/context/AuthContext';  //named import from AuthContext
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* Routes */}
     <BrowserRouter>
       {/* CONTEXT PROVIDER  */}
       <AuthContextProvider>
